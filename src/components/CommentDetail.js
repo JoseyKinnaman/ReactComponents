6e27494363cae95089ade7b9bc883db73e1f8367
Josey Kinnaman <josey.kinnaman@planetargon.com>
2020-08-28 17:03:02 -0700

    change CommentDetail to a jsx tag and import through index.js.

--- src/components/CommentDetail.js
+++ src/components/CommentDetail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import faker from 'faker';
 
 const CommentDetail = () => {
     return (
@@ -15,4 +16,6 @@ const CommentDetail = () => {
         </div>
       </div>
     )
-}
+};
+
+export default CommentDetail;
