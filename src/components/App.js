b22e21f3820fb51a42416ce8367e2481a99e03e4
Josey Kinnaman <josey.kinnaman@planetargon.com>
2020-11-18 17:20:38 -0800

    Add async API call.

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import './App.css';
 import './Accordion'
-import Accordion from './Accordion';
+// import Accordion from './Accordion';
 import Search from './Search';
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a front end Javascript framework'
-  },
-  {
-    title: 'Why use React?',
-    content: 'React is a favorite Javascript framework among engineers'
-  },
-  {
-    title: 'How do you use React?',
-    content: 'You use React by creating components'
-  },
-]
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a front end Javascript framework'
+//   },
+//   {
+//     title: 'Why use React?',
+//     content: 'React is a favorite Javascript framework among engineers'
+//   },
+//   {
+//     title: 'How do you use React?',
+//     content: 'You use React by creating components'
+//   },
+// ]
 
 export default () => {
   return <div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,14 +2,56 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm ] = useState('');
+  const [term, setTerm ] = useState('programming');
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
     const search = async () => {
-      await axios.get('')
+    const { data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action:'query',
+          list: 'search',
+          origin:'*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search)
     };
-  search();
-  }, [term]);
+      if (term && !results.length){
+        search();
+      } else {
+      const timeoutID = setTimeout(() =>{
+        if (term){
+          search();
+        }
+      }, 1000);
+      return () => {
+        clearTimeout(timeoutID);
+      };
+    }
+  }, [term, results.length]);
+
+  const renderedResults = results.map((result) => {
+    return(
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go!
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">
+          {result.title}
+          </div>
+          <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
+        </div>
+      </div>
+    )
+  })
 
   return(
     <div>
@@ -23,6 +65,9 @@ const Search = () => {
             onChange={e => setTerm(e.target.value)}
           />
         </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
       </div>
     </div>
   )
