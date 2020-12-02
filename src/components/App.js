1ff4bf51ac4328bdaa928ac7b261cff09f76376c
Josey Kinnaman <josey.kinnaman@planetargon.com>
2020-12-01 17:40:47 -0800

    add code for opening and closing the dropdown.

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 import './App.css';
 import './Accordion'
-// import Accordion from './Accordion';
-// import Search from './Search';
+import Accordion from './Accordion';
+import Search from './Search';
 import Dropdown from './Dropdown'
 
   const options = [
@@ -20,20 +20,20 @@ import Dropdown from './Dropdown'
     }
   ];
 
-// const items = [ß
-//   {
-//     title: 'What is React?',
-//     content: 'React is a front end Javascript framework'
-//   },
-//   {
-//     title: 'Why use React?',
-//     content: 'React is a favorite Javascript framework among engineers'
-//   },
-//   {
-//     title: 'How do you use React?',
-//     content: 'You use React by creating components'
-//   },
-// ]
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end Javascript framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite Javascript framework among engineers'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components'
+  },
+]
 
 export default () => {
   const [selected, setSelected] = useState(options[0])
@@ -43,6 +43,12 @@ export default () => {
     selected={selected}
     onSelectedChange={setSelected}
     options={options}/>
+    <br/>
+    <br/>
+  <Accordion items={items} />
+    <br/>
+  <Search />
+    <br/>
   </div>
 }
 
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React,{ useState, useEffect, useRef } from 'react';
 
 
 const Dropdown = ({options, selected, onSelectedChange}) => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener('click',(event) => {
+      if (ref.current.contains(event.target)){
+        return
+      }
+      setOpen(false);
+    });
+  }, []);
+
   const renderedOption = options.map((option) =>{
+    if(option.value === selected.value){
+      return null;
+    }
   return(
     <div
       key={option.value}
@@ -15,17 +30,20 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
 })
 
   return(
-  <div className="ui form">
+  <div ref ={ref} className="ui form">
     <div className="field">
       <label className="label">Select a Color</label>
-      <div className="ui selection dropdown visible active">
-          <i className="dropdown icon"></i>
-          <div className="text">
-            {selected.label}
-          </div>
-          <div className="menu visible transition">
-            {renderedOption}
-          </div>
+      <div
+        onClick={() => setOpen(!open)}
+        className={`ui selection dropdown ${open? 'visible active' : ''}`}
+      >
+        <i className="dropdown icon" />
+        <div className="text">
+          {selected.label}
+        </div>
+        <div className={`menu ${open ? 'visible transition' : ''}`}>
+          {renderedOption}
+        </div>
       </div>
     </div>
   </div>
