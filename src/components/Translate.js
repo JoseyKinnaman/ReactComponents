d1095766445abb1f2cdc501ee202a79f608a149b
Josey Kinnaman <josey.kinnaman@planetargon.com>
2020-12-10 15:25:05 -0800

    add translate app.

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,10 @@
 import React, {useState } from 'react';
 import Dropdown from './Dropdown';
+import Convert from './Convert';
+
+// AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
+
+
 
 const options = [
   {
@@ -33,6 +38,12 @@ const Translate = () => {
         selected={language}
         onSelectedChange={setLanguage}
         options={options}
+      />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert
+        text={text}
+        language={language}
       />
     </div>
   )
